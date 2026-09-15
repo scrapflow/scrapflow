589fe9e0b4519eb2ffe2f15a269c1db693abce18
Petrica Nanca <petrica_nanca@yahoo.com>
2026-09-15 16:13:07 +0300

    Add db interaction

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,21 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from app.core.database import engine, Base
+from app.models.user import User  
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("Initialize connection and table creation...")
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+        
+    yield
+    
+    print("Closing the DB connections...")
+    await engine.dispose()
+    print("The app is safely closed.")
+
+app = FastAPI(lifespan=lifespan)
 
 @app.get("/")
 def read_root():
---
+++ backend/app/models/user.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, String, Boolean, DateTime
+from sqlalchemy.sql import func
+from app.core.database import Base
+
+class User(Base):
+    """
+    Modelul SQLAlchemy pentru tabela 'users' din baza de date.
+    Moștenește din clasa 'Base' definită în database.py.
+    """
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    is_active = Column(Boolean, default=True, nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
